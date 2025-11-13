f428eb1862460edb79d475ab90c183df3a64cf08
ansifpk <pkansif39@gmail.com>
2025-11-13 12:33:13 +0530

    removed unwanted slice

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit';
 import authReducer from './slices/authSlices';
 import { apiSlice } from './slices/apiSlice';
-// import productsSlice from './slices/productsSlice';
 
 const store = configureStore({
     reducer:{
      auth:authReducer,
      [apiSlice.reducerPath]:apiSlice.reducer,
-    //  producs:productsSlice,
     },
     middleware:(getDefaultMiddleware)=>getDefaultMiddleware().concat(apiSlice.middleware),
     devTools:true
